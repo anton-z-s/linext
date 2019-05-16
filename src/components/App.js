9edbe2d58fbb8168313a4ebc2c3cdf25f3ec14cc
anthony <anton-z-s@users.noreply.github.com>
2019-05-16 18:58:22 +0800

    Don't limit max site width

--- src/components/App.js
+++ src/components/App.js
@@ -21,12 +21,7 @@ const styles = theme => ({
   container: {
     width: "auto",
     marginLeft: theme.spacing.unit * 3,
-    marginRight: theme.spacing.unit * 3,
-    [theme.breakpoints.up(1100 + theme.spacing.unit * 3 * 2)]: {
-      width: 1100,
-      marginLeft: "auto",
-      marginRight: "auto"
-    }
+    marginRight: theme.spacing.unit * 3
   }
 });
 
