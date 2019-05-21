fa6bc40b1d1c3a0d000c37e7528fbe32dd744489
anton-z-s <anton-z-s@users.noreply.github.com>
2019-05-21 21:08:28 +0800

    Disable pagination

--- src/components/DevicesTable.js
+++ src/components/DevicesTable.js
@@ -400,7 +400,7 @@ class DevicesTable extends Component {
           noDataText={loading ? "" : "No devices found..."}
           loading={loading}
           showPagination={false}
-          defaultPageSize={300}
+          pageSize={data.length}
           minRows={5}
           defaultSorted={[
             {
