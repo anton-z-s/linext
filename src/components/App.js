4d1ce3131ac90e7c2869121ca2a0ca782b8fa0b2
anton-z-s <anton-z-s@users.noreply.github.com>
2019-05-23 17:07:35 +0800

    Change theme color

--- src/components/App.js
+++ src/components/App.js
@@ -9,7 +9,9 @@ import {
   withStyles,
   IconButton,
   SvgIcon,
-  Link
+  Link,
+  createMuiTheme,
+  MuiThemeProvider
 } from "@material-ui/core";
 import { Smartphone, ViewColumn } from "@material-ui/icons";
 import DevicesTable from "./DevicesTable";
@@ -17,7 +19,6 @@ import DevicesTable from "./DevicesTable";
 const styles = theme => ({
   appBar: {
     position: "relative",
-    backgroundColor: "#167C80",
     marginBottom: theme.spacing.unit * 3
   },
   icon: {
@@ -33,57 +34,66 @@ const styles = theme => ({
   columnIconInText: { verticalAlign: "middle" }
 });
 
+const theme = createMuiTheme({
+  palette: {
+    primary: { main: "#167c80" },
+    secondary: { main: "#008e9e" }
+  }
+});
+
 function App(props) {
   const { classes, apolloClient } = props;
 
   return (
     <HashRouter basename="/">
       <CssBaseline />
-      <AppBar position="static" className={classes.appBar}>
-        <Toolbar>
-          <Smartphone className={classes.icon} />
-          <Typography
-            className={classes.appName}
-            variant="h6"
-            color="inherit"
-            noWrap
-          >
-            Linext
+      <MuiThemeProvider theme={theme}>
+        <AppBar position="static" className={classes.appBar}>
+          <Toolbar>
+            <Smartphone className={classes.icon} />
+            <Typography
+              className={classes.appName}
+              variant="h6"
+              color="inherit"
+              noWrap
+            >
+              Linext
+            </Typography>
+            <IconButton
+              href="https://github.com/anton-z-s/linext/"
+              target="_blank"
+              rel="noopener"
+              color="inherit"
+            >
+              <SvgIcon>
+                <path d="M12 .3a12 12 0 0 0-3.8 23.4c.6.1.8-.3.8-.6v-2c-3.3.7-4-1.6-4-1.6-.6-1.4-1.4-1.8-1.4-1.8-1-.7.1-.7.1-.7 1.2 0 1.9 1.2 1.9 1.2 1 1.8 2.8 1.3 3.5 1 0-.8.4-1.3.7-1.6-2.7-.3-5.5-1.3-5.5-6 0-1.2.5-2.3 1.3-3.1-.2-.4-.6-1.6 0-3.2 0 0 1-.3 3.4 1.2a11.5 11.5 0 0 1 6 0c2.3-1.5 3.3-1.2 3.3-1.2.6 1.6.2 2.8 0 3.2.9.8 1.3 1.9 1.3 3.2 0 4.6-2.8 5.6-5.5 5.9.5.4.9 1 .9 2.2v3.3c0 .3.1.7.8.6A12 12 0 0 0 12 .3" />
+              </SvgIcon>
+            </IconButton>
+          </Toolbar>
+        </AppBar>
+        <main className={classes.container}>
+          <Typography variant="h6" gutterBottom>
+            This is a list of{" "}
+            <Link
+              href="https://www.lineageos.org/"
+              target="_blank"
+              rel="noopener"
+            >
+              {" "}
+              LineageOS
+            </Link>{" "}
+            devices, presented in a convenient way.
           </Typography>
-          <IconButton
-            href="https://github.com/anton-z-s/linext/"
-            target="_blank"
-            rel="noopener"
-            color="inherit"
-          >
-            <SvgIcon>
-              <path d="M12 .3a12 12 0 0 0-3.8 23.4c.6.1.8-.3.8-.6v-2c-3.3.7-4-1.6-4-1.6-.6-1.4-1.4-1.8-1.4-1.8-1-.7.1-.7.1-.7 1.2 0 1.9 1.2 1.9 1.2 1 1.8 2.8 1.3 3.5 1 0-.8.4-1.3.7-1.6-2.7-.3-5.5-1.3-5.5-6 0-1.2.5-2.3 1.3-3.1-.2-.4-.6-1.6 0-3.2 0 0 1-.3 3.4 1.2a11.5 11.5 0 0 1 6 0c2.3-1.5 3.3-1.2 3.3-1.2.6 1.6.2 2.8 0 3.2.9.8 1.3 1.9 1.3 3.2 0 4.6-2.8 5.6-5.5 5.9.5.4.9 1 .9 2.2v3.3c0 .3.1.7.8.6A12 12 0 0 0 12 .3" />
-            </SvgIcon>
-          </IconButton>
-        </Toolbar>
-      </AppBar>
-      <main className={classes.container}>
-        <Typography variant="h6" gutterBottom>
-          This is a list of{" "}
-          <Link
-            href="https://www.lineageos.org/"
-            target="_blank"
-            rel="noopener"
-          >
-            {" "}
-            LineageOS
-          </Link>{" "}
-          devices, presented in a convenient way.
-        </Typography>
-        <Typography variant="subtitle1" gutterBottom>
-          You can sort (hold Shift to multi-sort), filter devices, show/hide
-          columns (
-          <ViewColumn className={classes.columnIconInText} />
-          ). All the info is straight out of the official wiki, always
-          up-to-date.
-        </Typography>
-        <DevicesTable apolloClient={apolloClient} />
-      </main>
+          <Typography variant="subtitle1" gutterBottom>
+            You can sort (hold Shift to multi-sort), filter devices, show/hide
+            columns (
+            <ViewColumn className={classes.columnIconInText} />
+            ). All the info is straight out of the official wiki, always
+            up-to-date.
+          </Typography>
+          <DevicesTable apolloClient={apolloClient} />
+        </main>
+      </MuiThemeProvider>
     </HashRouter>
   );
 }
