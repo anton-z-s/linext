64a3c6ff0b76fa8b13b928c55464290471b42cef
anton-z-s <anton-z-s@users.noreply.github.com>
2019-07-12 13:37:03 +0800

    Add PWA manifest

--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ import "typeface-roboto";
 import ApolloClient from "apollo-boost";
 import { ApolloProvider } from "react-apollo";
 import App from "./components/App";
+import * as serviceWorker from "./serviceWorker";
 
 const apolloClient = new ApolloClient({
   uri: "https://api.github.com/graphql",
@@ -26,3 +27,8 @@ ReactDOM.render(
   <App apolloClient={apolloClient} />,
   document.getElementById("root")
 );
+
+// If you want your app to work offline and load faster, you can change
+// unregister() to register() below. Note this comes with some pitfalls.
+// Learn more about service workers: https://bit.ly/CRA-PWA
+serviceWorker.register();
